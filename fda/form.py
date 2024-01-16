efe2cfb4165a9b648ceea47a478b1543f3624c7b
tiliguzova <141358684+tiliguzova@users.noreply.github.com>
2024-01-16 18:42:56 +0300

    Update form.py

--- fda/form.py
+++ fda/form.py
@@ -3,7 +3,7 @@ from django import forms
 from info.models import Info
 
 
-class InfoForm(forms.ModelFrom):
+class InfoForm(forms.ModelForm):
     class Meta:
         model = Info
         fields = ('name', 'text', 'rating', 'price')
